ff18b5608d94d0a7be02408f62dd757a7d963146
Your Name <you@example.com>
2021-09-02 17:33:10 +0300

    EndVersion

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using ConsoleApp15;
+using Dapper;
+using Npgsql;
+
+namespace Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection("User ID=user1;Password=changeme;Host=130.193.55.186;Port=5432;Database=tododb;");
+
+            try
+            {
+                Console.WriteLine("Введите имя");
+
+                string userName = Console.ReadLine();
+                while (string.IsNullOrEmpty(userName))
+                {
+                    Console.WriteLine("Недопустимое имя");
+                    userName = Console.ReadLine();
+                }
+                User user = new(userName);
+                Console.WriteLine("Введите сообщение");
+                Console.WriteLine("Для закрытия чата пропишите <Закрыть чат>");
+                UpdatTimer tm = new UpdatTimer();
+                
+                while (true)
+                {
+                        string messages = Console.ReadLine();
+                        if (string.IsNullOrEmpty(messages))
+                        {
+                            Console.WriteLine("Пустое сообщение");
+                            messages = Console.ReadLine();
+                        }
+                        if (messages == "Закрыть чат")
+                    {
+                        Environment.Exit(0);
+                    }
+                    connection.Query($"Insert into \"Messages\" (\"Name\", \"Message\", \"Time\") values ('{user.Name}', '{messages}', '{DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm")}')");
+                }
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+    }
+}
---
+++ ConsoleApp15/UpdatTimer.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp15
+{
+   public class UpdatTimer
+    {
+        public Timer Timer;
+        public UpdatTimer()
+        {
+            Timer = new Timer(TimerChat, null, 5000, 5000);
+        }
+
+    public void TimerChat(object o)
+    {
+        NpgsqlConnection Connection = new NpgsqlConnection("User ID=user1;Password=changeme;Host=130.193.55.186;Port=5432;Database=tododb;");
+        try
+        {
+            Console.Clear();
+            List<Friend> messages = Connection.Query<Friend>("SELECT * FROM \"Messages\" Order by \"Id\"").ToList();
+            foreach (Friend friend in messages)
+            {
+                Console.WriteLine(friend);
+            }
+        }
+        finally
+        {
+            Connection.Dispose();
+        }
+    }
+}
+}
